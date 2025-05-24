dc00328b1fbd0ba121f41049aa92275319d71b1c
Larips(Home) <laripsspiral47@gmail.com>
2025-05-24 23:43:03 +0700

    static page

--- src/app/(routes)/projects/[id]/page.tsx
+++ src/app/(routes)/projects/[id]/page.tsx
@@ -4,12 +4,6 @@ import { useParams } from "next/navigation";
 import { Projects } from "@/app/data/projectDB";
 import ProjectInspect from "@/app/components/ProjectInspect";
 
-// Generate static params for dynamic routes
-export async function generateStaticParams() {
-  const ids = Projects.map(project => encodeURIComponent(project.title));
-  return ids.map(id => ({ id }));
-}
-
 export default function ProjectView() {
   const { id } = useParams();
 
@@ -18,7 +12,7 @@ export default function ProjectView() {
 
   return (
     <div className="min-h-screen p-8">
-      <ProjectInspect {...inspectingProject}/>
+      <ProjectInspect {...inspectingProject} />
     </div>
   );
 }
