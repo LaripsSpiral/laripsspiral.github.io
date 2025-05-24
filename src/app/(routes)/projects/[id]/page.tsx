9f8a3468e495c1031b608b61a49d33ff45049d8c
Larips(Home) <laripsspiral47@gmail.com>
2025-05-24 16:50:57 +0700

    fix deploy

--- src/app/(routes)/projects/[id]/page.tsx
+++ src/app/(routes)/projects/[id]/page.tsx
@@ -1,42 +1,13 @@
-"use client";
-
-import { useParams } from "next/navigation";
-import { getProjects } from "@/app/lib/project/Queuery";
-
-import { ProjectInterface } from "@/app/lib/project/Interface";
-import ProjectInspect from "@/app/components/ProjectInspect";
-
-import { useState, useEffect } from "react";
-import { notFound } from "next/navigation";
+import ProjectView from "@/app/components/ProjectView";
+import { getProjectsFromDb } from "@/app/lib/project/service";
 
 export async function generateStaticParams() {
-  const projects = await getProjects();
+  const projects = getProjectsFromDb();
   return projects.map((project) => ({
     id: encodeURIComponent(project.title),
   }));
 }
 
 export default function ProjectPage() {
-  const { id } = useParams();
-  const [project, setProject] = useState<ProjectInterface | null>(null);
-
-  useEffect(() => {
-    const loadProject = async () => {
-      const projects = await getProjects();
-      const found = projects.find(
-        (p) => p.title === decodeURIComponent(String(id))
-      );
-      if (!found) return notFound();
-      setProject(found);
-    };
-    loadProject();
-  }, [id]);
-
-  if (!project) return null; // or a loading spinner, etc.
-
-  return (
-    <div className="min-h-screen p-8">
-      <ProjectInspect project={project} />
-    </div>
-  );
+  return <ProjectView />;
 }
