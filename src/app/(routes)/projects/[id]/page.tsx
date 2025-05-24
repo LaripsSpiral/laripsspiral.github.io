77e3e9bda1c83af2cbb2d211d15a1bc23c2b20f2
Larips(Home) <laripsspiral47@gmail.com>
2025-05-24 21:59:06 +0700

    creat sql yml

--- src/app/(routes)/projects/[id]/page.tsx
+++ src/app/(routes)/projects/[id]/page.tsx
@@ -1,12 +1,4 @@
 import ProjectView from "@/app/components/ProjectView";
-import { getProjectsFromDb } from "@/app/lib/project/service";
-
-export async function generateStaticParams() {
-  const projects = getProjectsFromDb();
-  return projects.map((project) => ({
-    id: encodeURIComponent(project.title),
-  }));
-}
 
 export default function ProjectPage() {
   return <ProjectView />;
