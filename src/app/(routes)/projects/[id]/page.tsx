ccd7b2e1ba716b6e508a8d7aab903a4fc9df9e2a
Larips(Home) <laripsspiral47@gmail.com>
2025-05-24 16:42:57 +0700

    static param

--- src/app/(routes)/projects/[id]/page.tsx
+++ src/app/(routes)/projects/[id]/page.tsx
@@ -1,13 +1,20 @@
 "use client";
 
 import { useParams } from "next/navigation";
+import { getProjects } from "@/app/lib/project/Queuery";
 
 import { ProjectInterface } from "@/app/lib/project/Interface";
-import { getProjects } from "@/app/lib/project/Queuery";
 import ProjectInspect from "@/app/components/ProjectInspect";
 
 import { useState, useEffect } from "react";
 import { notFound } from "next/navigation";
+
+export async function generateStaticParams() {
+  const projects = await getProjects();
+  return projects.map((project) => ({
+    id: encodeURIComponent(project.title),
+  }));
+}
 
 export default function ProjectPage() {
   const { id } = useParams();
