859812c91d2255ca529381ca24b2cefa13b28193
Larips(Home) <laripsspiral47@gmail.com>
2025-05-26 20:39:37 +0700

    Update page.tsx

--- src/app/(routes)/projects/[id]/page.tsx
+++ src/app/(routes)/projects/[id]/page.tsx
@@ -4,6 +4,12 @@ import { useParams } from "next/navigation";
 import { Projects } from "@/app/data/projectDB";
 import ProjectInspect from "@/app/components/ProjectInspect";
 
+export function generateStaticParams() {
+  return Projects.map(project => ({
+    id: encodeURIComponent(project.title),
+  }));
+}
+
 export default function ProjectView() {
   const params = useParams();
   const id = Array.isArray(params?.id) ? params.id[0] : params?.id;
